c7510b206d1eedd3356da316bffe71097d3a0cd2
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-30 18:50:10 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190430184539) do
+ActiveRecord::Schema.define(version: 20190430184743) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "payment_status"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
